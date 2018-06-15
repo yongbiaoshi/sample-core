8d87b17f29acaf82f38d0260f7828f407ba28d36
shiyongbiao@incar100.com <shiyongbiao@incar100.com>
2018-06-15 21:08:04 +0800

    修改异常封装

--- src/main/java/com/my/sample/core/config/exception/AppException.java
+++ src/main/java/com/my/sample/core/config/exception/AppException.java
@@ -2,23 +2,10 @@ package com.my.sample.core.config.exception;
 
 public class AppException extends RuntimeException {
 
-    /**
-     * 
-     */
-    private static final long serialVersionUID = -2285739833204427158L;
-
     public static final String DEFAULT_ERROR_CODE = "SYSTEM_ERROR";
     public static final String DEFAULT_ERROR_MSG = "系统错误";
-
+    private static final long serialVersionUID = -2285739833204427158L;
     private String errorCode;
-
-    public String getErrorCode() {
-        return errorCode;
-    }
-
-    public void setErrorCode(String errorCode) {
-        this.errorCode = errorCode;
-    }
 
     public AppException(String errorCode, String message) {
         super(message, null);
@@ -30,13 +17,11 @@ public class AppException extends RuntimeException {
         this.errorCode = errorCode;
     }
 
-    public AppException(String errorCode) {
-        super(AppErrors.message(errorCode), null);
-        this.errorCode = errorCode;
+    public String getErrorCode() {
+        return errorCode;
     }
 
-    public AppException(String errorCode, Throwable t) {
-        super(AppErrors.message(errorCode), t);
+    public void setErrorCode(String errorCode) {
         this.errorCode = errorCode;
     }
 }
