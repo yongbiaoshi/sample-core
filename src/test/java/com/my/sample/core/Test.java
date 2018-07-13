fe5bbab8d1b3f62808d1cbb0ec665373ee6d76ac
shiyongbiao@incar100.com <shiyongbiao@incar100.com>
2018-07-13 10:41:04 +0800

    修改日志格式

--- src/test/java/com/my/sample/core/Test.java
+++ src/test/java/com/my/sample/core/Test.java
@@ -1,8 +1,9 @@
 package com.my.sample.core;
 
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.temporal.TemporalField;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Test {
@@ -10,5 +11,26 @@ public class Test {
     public static void main(String[] args) {
         AtomicInteger ai = new AtomicInteger(0);
         int day = LocalDate.now().getDayOfYear();
+
+        ExecutorService servcie = Executors.newFixedThreadPool(10);
+
+        for (int i = 0; i < 10; i++) {
+            final int j = i;
+            servcie.execute(() -> {
+                try {
+                    Thread.sleep(100);
+                    System.out.println("执行操作" + j);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        servcie.shutdown();
+        try {
+            servcie.awaitTermination(30, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("结束");
     }
 }
