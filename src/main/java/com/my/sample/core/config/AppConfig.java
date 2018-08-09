623d77347276ba2be40273a7293d2912fdac8dc0
shiyongbiao@incar100.com <shiyongbiao@incar100.com>
2018-08-09 12:18:13 +0800

    添加feign测试

--- src/main/java/com/my/sample/core/config/AppConfig.java
+++ src/main/java/com/my/sample/core/config/AppConfig.java
@@ -4,6 +4,7 @@ import com.spring4all.swagger.EnableSwagger2Doc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableSwagger2Doc // 开启Swagger2，自动生成文档，Url：/swagger-ui.html
 @EnableConfigurationProperties(AppProperties.class)
 @EnableAsync
+@EnableFeignClients(basePackages = {"com.my.sample.core.service"})
 public class AppConfig {
 
 }
