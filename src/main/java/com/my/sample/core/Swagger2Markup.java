3af4c2f7eb50857d15ad82dcc3a488dec393c9b8
shiyongbiao <shyb0904@163.com>
2018-08-14 00:47:21 +0800

    swagger生成markdown代码

--- src/main/java/com/my/sample/core/Swagger2Markup.java
+++ src/main/java/com/my/sample/core/Swagger2Markup.java
@@ -7,18 +7,21 @@ import io.github.swagger2markup.markup.builder.MarkupLanguage;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Swagger2Markup {
 
     public static void main(String[] args) throws MalformedURLException {
         Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
-                .withMarkupLanguage(MarkupLanguage.MARKDOWN)
                 .build();
 
-        Swagger2MarkupConverter.from(new URL("http://localhost:8080/v2/api-docs"))
+        Path path = Paths.get("E:/api-docs.json");
+        URL url = new URL("http://localhost:8080/v2/api-docs");
+        Swagger2MarkupConverter.from(path)
                 .withConfig(config)
                 .build()
-                .toFolder(Paths.get("src/docs/markdown/generated"));
+                .toFile(Paths.get("src/docs/markdown/generated/api-docs"));
+//                .toFolder(Paths.get("src/docs/markdown/generated"));
     }
 }
