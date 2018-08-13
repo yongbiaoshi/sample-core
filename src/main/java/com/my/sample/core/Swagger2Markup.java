4ea8bf1c764334797bcd60f6cf609195e7712b36
shiyongbiao <shyb0904@163.com>
2018-08-13 23:17:24 +0800

    添加使用Swagger生成Markdown文档的功能

---
+++ src/main/java/com/my/sample/core/Swagger2Markup.java
@@ -0,0 +1,24 @@
+package com.my.sample.core;
+
+import io.github.swagger2markup.Swagger2MarkupConfig;
+import io.github.swagger2markup.Swagger2MarkupConverter;
+import io.github.swagger2markup.builder.Swagger2MarkupConfigBuilder;
+import io.github.swagger2markup.markup.builder.MarkupLanguage;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Paths;
+
+public class Swagger2Markup {
+
+    public static void main(String[] args) throws MalformedURLException {
+        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+                .withMarkupLanguage(MarkupLanguage.MARKDOWN)
+                .build();
+
+        Swagger2MarkupConverter.from(new URL("http://localhost:8080/v2/api-docs"))
+                .withConfig(config)
+                .build()
+                .toFolder(Paths.get("src/docs/markdown/generated"));
+    }
+}
